f574124204b5c64dc0d10f6a1169a57b500d7ee6
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-02-03 20:41:09 +0330

    version 1.00, AC & M don't work yet

--- calculator.cpp
+++ calculator.cpp
@@ -22,6 +22,13 @@ Calculator::Calculator(QWidget *parent)
         connect(numButtons[i], SIGNAL(released()), this, SLOT(NumPressed()));
     }
 
+    connect(ui->Add, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Subtract, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Multiply, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Divide, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Equals, SIGNAL(released()), this, SLOT(EqualButton()));
+    connect(ui->ChangeSign, SIGNAL(released()), this, SLOT(ChangeNumberSign()));
+
 }
 
 Calculator::~Calculator()
@@ -48,5 +55,71 @@ void Calculator::NumPressed()
 
 void Calculator::MathButtonPressed()
 {
+    divTrigger = false;
+    multTrigger = false;
+    addTrigger = false;
+    subTrigger = false;
+    QString displayVal = ui->Display->text();
+    calval = displayVal.toDouble();
+    QPushButton* button = (QPushButton* )sender();
+    QString butVal = button->text();
+    if (QString::compare(butVal, "/", Qt::CaseInsensitive) == 0)
+    {
+        divTrigger = true;
+    }
+    else if (QString::compare(butVal, "*", Qt::CaseInsensitive) == 0)
+    {
+        multTrigger = true;
+    }
+    else if (QString::compare(butVal, "+", Qt::CaseInsensitive) == 0)
+    {
+        addTrigger = true;
+    }
+    else if (QString::compare(butVal, "-", Qt::CaseInsensitive) == 0)
+    {
+        subTrigger = true;
+    }
+    ui->Display->setText("");
+
+}
+
+void Calculator::EqualButton()
+{
+    double solution = 0.0;
+    QString displayVal = ui->Display->text();
+    double dblDisplayVal = displayVal.toDouble();
+    if (addTrigger || subTrigger || divTrigger || multTrigger)
+    {
+        if (addTrigger)
+        {
+            solution = calval + dblDisplayVal;
+        }
+        else if (subTrigger)
+        {
+            solution = calval - dblDisplayVal;
+        }
+        else if (multTrigger)
+        {
+            solution = calval * dblDisplayVal;
+        }
+        else
+        {
+            solution = calval / dblDisplayVal;
+        }
+    }
+    ui->Display->setText(QString::number(solution));
+
+}
+
+void Calculator::ChangeNumberSign()
+{
+    QString displayVal = ui->Display->text();
+    QRegExp reg("[-]?[0-9.]*");
+    if (reg.exactMatch(displayVal))
+    {
+        double dblDisplayVal = displayVal.toDouble();
+        double dblDisplayValSign = -1 * dblDisplayVal;
+        ui->Display->setText(QString::number(dblDisplayValSign));
+    }
 
 }
